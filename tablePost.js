7a565937ecd3089715265b774fd543037f5ef81f
Jordan Wallet <JordanW@axosoft.com>
2018-08-28 14:21:41 -0700

    Fix 'post' table offsets

--- tablePost.js
+++ tablePost.js
@@ -1,11 +1,15 @@
 
 const table = require('./table');
 
-const FORMAT_OFFSET             = 0,
-      ITALIC_ANGLE_OFFSET       = FORMAT_OFFSET + 4;
-      UNDERLINE_POSITION_OFFSET = ITALIC_ANGLE_OFFSET + 8,
-      UNDERLINE_THICKNESS_OFFSET = UNDERLINE_POSITION_OFFSET + 2;
-      IS_FIXED_PITCH_OFFSET      = UNDERLINE_THICKNESS_OFFSET + 2;
+const FORMAT_OFFSET              = 0,
+      ITALIC_ANGLE_OFFSET        = FORMAT_OFFSET + 4;
+      UNDERLINE_POSITION_OFFSET  = ITALIC_ANGLE_OFFSET + 4,
+      UNDERLINE_THICKNESS_OFFSET = UNDERLINE_POSITION_OFFSET + 2,
+      IS_FIXED_PITCH_OFFSET      = UNDERLINE_THICKNESS_OFFSET + 2,
+      MIN_MEM_TYPE_42_OFFSET     = IS_FIXED_PITCH_OFFSET + 4,
+      MAX_MEM_TYPE_42_OFFSET     = MIN_MEM_TYPE_42_OFFSET + 4,
+      MIN_MEM_TYPE_1_OFFSET      = MAX_MEM_TYPE_42_OFFSET + 4,
+      MAX_MEM_TYPE_1_OFFSET      = MIN_MEM_TYPE_1_OFFSET + 4;
 
 var fixed16dot16 = function(fixed) {
   if (fixed & 0x80000000) {
@@ -25,11 +29,9 @@ module.exports = function(data) {
     underlinePosition : data.readInt16BE(o+UNDERLINE_POSITION_OFFSET),
     underlineThickness: data.readInt16BE(o+UNDERLINE_THICKNESS_OFFSET),
     isFixedPitch      : data.readUInt32BE(o+IS_FIXED_PITCH_OFFSET),
-    minMemType42      : data.readUInt32BE(o+7),
-    maxMemType42      : data.readUInt32BE(o+9),
-    minMemType1       : data.readUInt32BE(o+11),
-    maxMemType1       : data.readUInt32BE(o+13)
+    minMemType42      : data.readUInt32BE(o+MIN_MEM_TYPE_42_OFFSET),
+    maxMemType42      : data.readUInt32BE(o+MAX_MEM_TYPE_42_OFFSET),
+    minMemType1       : data.readUInt32BE(o+MIN_MEM_TYPE_1_OFFSET),
+    maxMemType1       : data.readUInt32BE(o+MAX_MEM_TYPE_1_OFFSET)
   };
 }
-
-
